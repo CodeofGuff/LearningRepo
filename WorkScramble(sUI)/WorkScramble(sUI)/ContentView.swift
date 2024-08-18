b8fafcc45df0ff8de3e1cbbc3858ddf406066b61
CodeofGuff <guffcode@gmail.com>
2024-08-18 13:10:28 -0700

    Learninggg timeeee

---
+++ WorkScramble(sUI)/WorkScramble(sUI)/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  WorkScramble(sUI)
+//
+//  Created by David Guffre on 8/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	var body: some View {
+		ZStack {
+			Color(red: 21/255, green: 21/255, blue:21/255).ignoresSafeArea()
+			
+			VStack {
+				Image(systemName: "macpro.gen1.fill")
+					.resizable()
+					.frame(width: 50, height: 50)
+					.foregroundStyle(.red)
+				Text("Hello, David")
+					.foregroundStyle(.yellow)
+					.font(.largeTitle)
+					.bold()
+			}
+		}
+	}
+}
+
+#Preview {
+	ContentView()
+}
---
+++ WorkScramble(sUI)/WorkScramble(sUI)/WorkScramble_sUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  WorkScramble_sUI_App.swift
+//  WorkScramble(sUI)
+//
+//  Created by David Guffre on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkScramble_sUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
