280f1143de6c7cbc46016be2cba94fb4d3d1f3a7
CodeofGuff <guffcode@gmail.com>
2024-09-01 12:09:24 -0700

    We learningg!

---
+++ Moonshot(sUI)/Moonshot(sUI)/Moonshot_sUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_sUI_App.swift
+//  Moonshot(sUI)
+//
+//  Created by David Guffre on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_sUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
