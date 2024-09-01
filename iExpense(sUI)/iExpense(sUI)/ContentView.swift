ba76dfa34c0f97826a1b2e7f9069854096e22365
CodeofGuff <guffcode@gmail.com>
2024-08-31 18:54:57 -0700

    Project 7 wrap up

--- iExpense(sUI)/iExpense(sUI)/ContentView.swift
+++ iExpense(sUI)/iExpense(sUI)/ContentView.swift
@@ -4,46 +4,97 @@
 //
 //  Created by David Guffre on 8/29/24.
 //
+
+
+
+
+// Challange
+// Rebrand with the red/grey/yellow colors
+// Split expeneses into Personal and Business
+// Contain styling / $$$ signs dpeending on dollar amount ($10, $100, $1000, etc)
+
+
 import SwiftUI
+
+
+struct ExpenseItem: Identifiable, Codable {
+	var id = UUID()
+	let name: String
+	let type: String
+	let amount: Double
+	
+}
+
+@Observable
+class Expenses {
+	var items = [ExpenseItem]() {
+		didSet {
+			if let encoded = try? JSONEncoder().encode(items) {
+				UserDefaults.standard.set(encoded, forKey: "Items")
+			}
+		}
+	}
+	
+	init() {
+		if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+			if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+				items = decodedItems
+				return
+			}
+		}
+		items = []
+	}
+	
+}
+
+
 
 struct ContentView: View {
 	@State private var animationAmount: Double = 0.0
 	@State var mainColor = Color(red: 30/255, green: 30/255, blue: 30/255)
 	@State private var showingSheet = false
 	
+	@State private var expenses = Expenses()
+	@State private var showingAddExpense = false
+	
+	
 	var body: some View {
-		ZStack {
-			mainColor.ignoresSafeArea()
-			
-			VStack {
-				Button(action: {
-					withAnimation {
-						animationAmount += 360
-						showingSheet.toggle()
+		NavigationStack {
+			List {
+				ForEach(expenses.items) { item in
+					HStack {
+						VStack(alignment: .leading) {
+							Text(item.name)
+								.font(.headline)
+							
+							Text(item.type)
+						}
+						Spacer()
+						
+						Text(item.amount, format: .currency(code: "USD"))
 					}
-				}) {
-					
-					Image(systemName: "macpro.gen1.fill")
-						.resizable()
-						.frame(width: 50, height: 50)
-						.foregroundStyle(.red)
-						.rotationEffect(.degrees(animationAmount))
-					
 				}
-				.sheet(isPresented: $showingSheet) {
-					SecondView()
+				.onDelete(perform: removeItems)
+			}
+			.navigationTitle("iExpense")
+			.toolbar {
+				Button("Add Expense", systemImage: "plus") {
+					showingAddExpense = true
 				}
-				Text("Hello David")
-					.font(.largeTitle)
-					.bold()
-					.foregroundStyle(.yellow)
+			}
+			.sheet(isPresented: $showingAddExpense) {
+				AddView(expenses: expenses)
 			}
 			
 		}
-		
-		
-		
 	}
+	
+	
+	func removeItems(at offsets: IndexSet) {
+		expenses.items.remove(atOffsets: offsets)
+	}
+	
+	
 }
 
 struct SecondView: View {
