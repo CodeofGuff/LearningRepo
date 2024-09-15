bc5f40c247ce2352d277a1396ec8c71d195453a5
CodeofGuff <guffcode@gmail.com>
2024-09-15 14:29:21 -0700

    Ok

--- Navigation(sUI)/Navigation(sUI)/ContentView.swift
+++ Navigation(sUI)/Navigation(sUI)/ContentView.swift
@@ -6,49 +6,23 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-	@State private var aniAmount: Double = 0.0
-	@State private var mainColor = Color(red: 30/255, green: 30/255, blue: 30/255)
-	@State private var showingSheet = false
-	
+struct DetailView: View {
+	var number: Int
 	var body: some View {
-		ZStack {
-			mainColor.ignoresSafeArea()
-			VStack {
-				Button(action: {
-					withAnimation {
-						aniAmount += 360
-						showingSheet.toggle()
-					}
-				}) {
-					Image(systemName: "macpro.gen1.fill")
-						.resizable()
-						.frame(width: 50, height: 50)
-						.foregroundStyle(.red)
-						.rotationEffect(.degrees(aniAmount))
-				}
-				.sheet(isPresented: $showingSheet) {
-					SecondView()
-				}
-				Text("Hello David")
-					.font(.largeTitle)
-					.bold()
-					.foregroundStyle(.yellow)
-			}
-		}
+		NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+			.navigationTitle("Number: \(number)")
 	}
 }
-struct SecondView: View {
-	@State private var mainColor = Color(red: 30/255, green: 30/255, blue: 30/255)
-	@Environment(\.dismiss) var dismiss
+
+struct ContentView: View {
+	@State private var path = [Int]()
+	
 	var body: some View {
-		ZStack {
-			mainColor.ignoresSafeArea()
-			Text("Error: ----->      immediately")
-			Button("exit") {
-				dismiss()
-			}
+		NavigationStack(path: $path) {
+			DetailView(number: 0)
+				.navigationDestination(for: Int.self) { i in
+					DetailView(number: i)
+				}
 		}
 	}
 }
