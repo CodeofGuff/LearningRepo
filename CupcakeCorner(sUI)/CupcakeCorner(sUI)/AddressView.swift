d462e18e364c63f4ab88b7f067881ab451bbf7a9
CodeofGuff <guffcode@gmail.com>
2024-09-17 14:39:32 -0700

    We learning

---
+++ CupcakeCorner(sUI)/CupcakeCorner(sUI)/AddressView.swift
@@ -0,0 +1,21 @@
+//
+//  AddressView.swift
+//  CupcakeCorner(sUI)
+//
+//  Created by David Guffre on 9/17/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+	
+	var order: Order
+	
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+	AddressView(order: Order())
+}
---
+++ CupcakeCorner(sUI)/CupcakeCorner(sUI)/CupcakeCorner_sUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_sUI_App.swift
+//  CupcakeCorner(sUI)
+//
+//  Created by David Guffre on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_sUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
